dc4690b6267aba5c5fa658d8f8d60af71b022718
WarriorSFG <samarthgupta9999@gmail.com>
2025-12-08 13:48:51 +0530

    Added Sampled Datasets
    Trimmed the dataset and added the first 2048 rows of each set, for viewing

---
+++ Scripts/Helper/Submissions_Sampler.py
@@ -0,0 +1,22 @@
+import zstandard as zstd
+import io
+import json
+
+database = "..\..\Datasets\submissions\RS_2024-07.zst"
+SampledSet = "..\..\SampledSet\submissions\RS_2024-07.jsonl"
+
+with open(database, 'rb') as db:
+    dctx = zstd.ZstdDecompressor()
+
+    with open(SampledSet, 'w+', encoding='utf-8') as f:
+        with dctx.stream_reader(db) as reader:
+            text_stream = io.TextIOWrapper(reader, encoding='utf-8')
+            count = 0
+            for line in text_stream:
+                f.write(line)
+                if count == 2048:
+                    break
+                else:
+                    count+=1
+                    
+print("Sampling complete.")
