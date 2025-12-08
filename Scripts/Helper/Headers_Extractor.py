b20995b8925656e7761ba0a97790df0464285c05
WarriorSFG <samarthgupta9999@gmail.com>
2025-12-08 14:08:56 +0530

    Add header extraction script and header lists
    Added Headers_Extractor.py to extract unique field names from Reddit comments and submissions datasets. Also added Headers.txt files listing extracted headers for both comments and submissions to SampledSet.

---
+++ Scripts/Helper/Headers_Extractor.py
@@ -0,0 +1,63 @@
+from multiprocessing.reduction import duplicate
+
+import zstandard as zstd
+import io
+import json
+
+databaseC = "..\..\Datasets\comments\RC_2024-07.zst"
+HeadersC =  "..\..\SampledSet\comments\Headers.txt"
+
+
+databaseS = "..\..\Datasets\submissions\RS_2024-07.zst"
+HeadersS =  "..\..\SampledSet\submissions\Headers.txt"
+
+Comment_Headers = []
+Submission_Headers = []
+
+with open(databaseC, 'rb') as db:
+    dctx = zstd.ZstdDecompressor()
+
+    with dctx.stream_reader(db) as reader:
+        text_stream = io.TextIOWrapper(reader, encoding='utf-8')
+
+        for i in range(2048):
+            line = next(text_stream)
+            data = json.loads(line)
+            for item in list(data.keys()):
+                if item in Comment_Headers:
+                    pass
+                else:
+                    Comment_Headers.append(item)
+
+print(Comment_Headers)
+
+with open(databaseS, 'rb') as db:
+    dctx = zstd.ZstdDecompressor()
+
+    with dctx.stream_reader(db) as reader:
+        text_stream = io.TextIOWrapper(reader, encoding='utf-8')
+
+        for i in range(2048):
+            line = next(text_stream)
+            data = json.loads(line)
+            for item in list(data.keys()):
+                if item in Submission_Headers:
+                    pass
+                else:
+                    Submission_Headers.append(item)
+
+print(Submission_Headers)
+
+with open(HeadersS, 'w+') as f:
+    for item in Submission_Headers:
+        f.write(item + '\n')
+
+duplicate = 0
+for item in Submission_Headers:
+    if item in Comment_Headers:
+        duplicate +=1
+
+print("Total comment fields :", len(Comment_Headers))
+print("Total submission fields :", len(Submission_Headers))
+print("Total duplicate fields :", duplicate)
+print("Extraction complete.")
